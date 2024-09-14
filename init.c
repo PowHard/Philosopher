25a6b696c0961a7f29aacc927e21e28293796f80
Pereira--Montero <esteban71.montero@gmail.com>
2024-09-14 11:34:20 +0200

    philo creation finish

--- init.c
+++ init.c
@@ -20,15 +20,18 @@ void	ft_init_threads(t_init *init, t_philo *philo)
 	int	i;
 
 	i = 0;
+	philo = malloc(sizeof(t_philo) * init->nb_philo);
 	while (i < init->nb_philo)
 	{
 		philo[i].philo_id = i + 1;
 		pthread_create(&philo[i].thread, NULL, ft_is_create, &philo[i].philo_id);
 		i++;
 	}
+	i = 0;
 	while (i < init->nb_philo)
 	{
 		pthread_join(philo[i].thread, NULL);
 		i++;
 	}
+	free(philo);
 }
--- philo.h
+++ philo.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 typedef struct s_init
 {
